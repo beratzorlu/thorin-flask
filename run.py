d45422a5fd2c35672acdbb1096ed50b9d096267d
berat.zorlu <berat.zorlu@hotmail.com>
2023-03-23 15:37:24 +0000

    Add Jinja templating and contact.html

--- run.py
+++ run.py
@@ -5,11 +5,24 @@ app = Flask(__name__)
 
 # Flask expects called html files to be in a file named templates.
 # The folder should be on the same level as the run.py file.
+# The @app.route("/") decorator allows the route and the index()
+# to be called together, one won't be called without the other.
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 # __main__ is the name of the default module of Python. We run this first,
 # it will be run directly.
