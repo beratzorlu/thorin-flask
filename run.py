68921c0b78f8ed2a155e86a05809726dbb3bd05e
berat.zorlu <berat.zorlu@hotmail.com>
2023-03-25 19:59:02 +0000

    Add app route to member.html

--- run.py
+++ run.py
@@ -23,6 +23,20 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+# Whatever comes after here will be passed into the parameter set in the view.
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    # First member is the variable name passed into member.html file
+    # Second member is the member object created earlier.
+    return render_template("member.html", member=member)
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
